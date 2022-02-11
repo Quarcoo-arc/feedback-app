8bc7189d5eb4f86463144d7562a5d2b840754634
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-11 14:44:51 +0000

    stylesheet added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>It's Mike!</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>Feedback App</h2>
+    </div>
+  );
+};
+
+export default Header;
