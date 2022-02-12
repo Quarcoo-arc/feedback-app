ac0931c8cf7e1830399faa067576275d1d1a1dd5
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-12 16:41:26 +0000

    About page added

--- src/Components/Pages/AboutPage.jsx
+++ src/Components/Pages/AboutPage.jsx
@@ -1,11 +1,19 @@
+import Card from "../Shared/Card";
+
 const AboutPage = () => {
   return (
-    <>
-      <div>AboutPage</div>
-      <div className="about-link">
-        <a href="/">{"<"}Back</a>
-      </div>
-    </>
+    <div className="container">
+      <Card>
+        <div className="about">
+          <h1>About This Project</h1>
+          <p>
+            This is a React app for leaving feedback for products and services
+          </p>
+          <p>Version: 1.0.0</p>
+          <a href="/">{"<"}Back</a>
+        </div>
+      </Card>
+    </div>
   );
 };
 
