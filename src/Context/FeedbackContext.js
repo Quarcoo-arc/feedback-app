7befa07242f8dfbc35302c07932850b12af2c67c
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-13 22:05:28 +0000

    created spinner component

--- src/Context/FeedbackContext.js
+++ src/Context/FeedbackContext.js
@@ -1,15 +1,30 @@
-import { useState, createContext } from "react";
-import FeedbackData from "../Data/FeedbackData";
+import { useState, createContext, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback()
+      .then((data) => setFeedback(data))
+      .then(() => setIsLoading(false));
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+    const data = response.json();
+    return data;
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete this item?")) {
@@ -30,10 +45,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
         updateFeedback,
       }}
     >
